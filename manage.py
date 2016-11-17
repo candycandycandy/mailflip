6926932820164aa341683124520398407713643c
Tommy Jebb <tjebb@steadfast.com>
2016-11-17 14:24:32 -0500

    Adding Procfile

--- manage.py
+++ manage.py
@@ -1,4 +1,13 @@
 from app import create_app
 from flask.ext.script import Manager
 
+
+app = create_app()
 manager = Manager(app)
+
+@manager.command
+def run():
+	app.run()
+
+if __name__ == "__main__":
+	manager.run()
